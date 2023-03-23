553323b6155d315ed613c9790a973ec05a1e8335
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-03-23 16:41:23 +0100

    Badge gradient

--- src/components/SourceBadge.tsx
+++ src/components/SourceBadge.tsx
@@ -1,6 +1,6 @@
 const SourceBadge = ({ label }: { label: string }) => {
   return (
-    <p className='my-2 w-fit bg-gradient-to-r from-zinc-500 to-zinc-400 px-2 py-1 text-xs font-semibold text-white'>
+    <p className='my-2 w-fit bg-gradient-to-r from-brandDarker to-zinc-700 px-2 py-1 text-xs font-semibold text-white'>
       {label}
     </p>
   );
