23c0a9694396f5a046f6e028d063c65f6014deb3
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-03-30 14:53:03 +0200

    Check me modal

--- src/components/Modals/CheckMeModal.tsx
+++ src/components/Modals/CheckMeModal.tsx
@@ -10,7 +10,15 @@ const CheckMeModal = () => {
 
   return (
     <Template toggleModal={() => dispatch(toggleModal())}>
-      <div className='p-4'>Największą trudność sprawiło mi:</div>
+      <div className='p-6 pt-16'>
+        Wykonując to zadanie nie napotkałem żadnych większych trudności. Są to technologie, w
+        których tworzę projekty na co dzień, często wykorzystuję też Next.js czy Gatsby. Ciesze się,
+        że robiąc to zadanie mogłem odświeżyć sobie np. bibliotekę react-router-dom oraz jej
+        integrację z react-query. Nowością była dla mnie biblioteka Format.js, ale wdrażanie jej do
+        projektu było proste i przyjemne. Ewentualne możliwości rozwoju strony, jakie widzę to np.:
+        paginacja artykułów (podział na strony lub infinite scroll), pobieranie artykułów po stronie
+        serwera (SSR) w celu poprawy SEO, jednak w tym celu wykorzystałbym już raczej Nextjs.
+      </div>
     </Template>
   );
 };
